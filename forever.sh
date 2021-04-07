e78edd1cefe4b86282af7bcc67b8129af3c727f6
Juan Cruz Martino <juan@gethenry.co>
2021-04-08 01:17:16 +0200

    update

--- forever.sh
+++ forever.sh
@@ -1,10 +1,2 @@
-#!/bin/bash
-cd /root/jcm/zenbot_jcm_1/
-docker-compose up -d mongodb
-sh forever.sh trade --paper
-cd /root/jcm/zenbot_jcm_2
-sh forever.sh trade --paper
-cd /root/juampi/zenbont_jp_1
-sh forever.sh trade --paper
-cd /root/juampi/zenbont_jp_2
-sh forever.sh trade --paper
+#!/bin/sh
+forever start zenbot.js $@
